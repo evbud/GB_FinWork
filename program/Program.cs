ce6a559f09ba3308f694f93a8a8204c0d5d39c58
Evgenii B <budich@me.com>
2022-10-05 23:41:47 +0300

    Added program folder

---
+++ program/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] array = FillArray();
+
+Console.Write($"[{String.Join(", ", array)}]");
+
+Console.WriteLine("\nИз заданного массива строк программа формирует массив из строк, длина которых меньше либо равна 3 символа:");
+
+Console.Write($"[{String.Join(", ", EditArray(array, 3))}]");
+
+string[] FillArray()
+{
+    Console.WriteLine("Введите через пробел значения элементов массива: ");
+    string arrVal = Console.ReadLine();
+
+    string[] array = arrVal.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    return array;
+}
+
+int CountElLessN(string[] array, int n)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n) count++;
+    }
+    return count;
+}
+
+string[] EditArray(string[] array, int n)
+{
+    string[] arrayEdited = new string[CountElLessN(array, n)];
+
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            arrayEdited[j] = array[i];
+            j++;
+        }
+    }
+    return arrayEdited;
+}
